4b735007fccbc8397f7bed4ec00b0962836f651a
Tamara_Grigoryan <toma_grigorian@mail.ru>
2022-01-31 23:36:49 +0300

    edit game_2.py

--- project_0/game_2.py
+++ project_0/game_2.py
@@ -1,31 +1,38 @@
-"Guess the number"
+"""Игра угадай число
+Компьютер сам загадывает и сам угадывает число
+"""
 
 import numpy as np
 
 
 def random_predict(number: int = 1) -> int:
-    """[summary]
+    """Рандомно угадываем число
 
     Args:
-        number (int, optional): [description]. Defaults to 1.
+        number (int, optional): Загаданное число. Defaults to 1.
 
     Returns:
-        int: [description]
+        int: Число попыток
     """
-
     count = 0
+
+    min_number = 1
+    max_number = 101
 
     while True:
         count += 1
-        predict_number = np.random.randint(1, 101)
-        if number == predict_number:
-            break
-
-    return(count)
+        predict_number = (min_number + max_number) // 2
+        if number < predict_number:
+            max_number = predict_number
+        elif number > predict_number:
+            min_number = predict_number
+        else:
+            break  # выход из цикла если угадали
+    return count
 
 
 def score_game(random_predict) -> int:
-    """За какое количество попыток в среднем угадывает наш подход
+    """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
 
     Args:
         random_predict ([type]): функция угадывания
@@ -34,14 +41,18 @@ def score_game(random_predict) -> int:
         int: среднее количество попыток
     """
     count_ls = []
-    np.random.seed(1)
-    random_array = np.random.randint(1, 101, size=(1000))
+    # np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(
+        1, 101, size=(1000))  # загадали список чисел
 
     for number in random_array:
         count_ls.append(random_predict(number))
+
     score = int(np.mean(count_ls))
-    print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
+    print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
     return score
 
 
-print(f'Количество попыток: {random_predict(10)}')
+if __name__ == "__main__":
+    # RUN
+    score_game(random_predict)
